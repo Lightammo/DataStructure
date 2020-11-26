f364a90211a36886a91da810fd1f0316b532502d
luoyuanlong <larryl786337611@outlook.com>
2020-11-27 00:31:04 +0800

    窗体设计

--- Week3/StackAndQueue/StackAndQueue/Form1.cs
+++ Week3/StackAndQueue/StackAndQueue/Form1.cs
@@ -16,5 +16,30 @@ namespace StackAndQueue
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
